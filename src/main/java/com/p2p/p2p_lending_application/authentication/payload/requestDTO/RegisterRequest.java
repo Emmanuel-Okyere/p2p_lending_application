c263c297e4a7f56233831f2ce5ec97a742066e60
emmanuel-gyateng <gyateng94@gmail.com>
2023-01-19 22:35:06 +0000

    Completed the registration of users and tests

---
+++ src/main/java/com/p2p/p2p_lending_application/authentication/payload/requestDTO/RegisterRequest.java
@@ -0,0 +1,20 @@
+package com.p2p.p2p_lending_application.authentication.payload.requestDTO;
+
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    private String username;
+    private String fullName;
+    @Email
+    private String emailAddress;
+    private String password;
+    private String confirmPassword;
+}
