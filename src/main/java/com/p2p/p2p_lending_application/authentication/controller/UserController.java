fd837f6c50f4af6a879af57642b735c993cce662
emmanuel-gyateng <gyateng94@gmail.com>
2023-01-20 17:57:31 +0000

    Added the refresh token

--- src/main/java/com/p2p/p2p_lending_application/authentication/controller/UserController.java
+++ src/main/java/com/p2p/p2p_lending_application/authentication/controller/UserController.java
@@ -1,22 +1,32 @@
 package com.p2p.p2p_lending_application.authentication.controller;
 
+import com.p2p.p2p_lending_application.authentication.exception.TokenRefreshException;
+import com.p2p.p2p_lending_application.authentication.model.RefreshToken;
 import com.p2p.p2p_lending_application.authentication.model.User;
 import com.p2p.p2p_lending_application.authentication.payload.requestDTO.LoginRequest;
+import com.p2p.p2p_lending_application.authentication.payload.requestDTO.RefreshTokenRequest;
 import com.p2p.p2p_lending_application.authentication.payload.requestDTO.RegisterRequest;
+import com.p2p.p2p_lending_application.authentication.payload.responseDTO.TokenRefreshResponse;
+import com.p2p.p2p_lending_application.authentication.service.RefreshTokenService;
 import com.p2p.p2p_lending_application.authentication.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("api/v1/auth/")
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private RefreshTokenService refreshTokenService;
     @PostMapping("register")
     public ResponseEntity<?> registerUser(@RequestBody @Valid RegisterRequest registerRequest){
         User user = new User(registerRequest.getUsername(),registerRequest.getFullName(), registerRequest.getEmailAddress(),registerRequest.getPassword(),registerRequest.getConfirmPassword());
@@ -27,5 +37,15 @@ public class UserController {
     public ResponseEntity<?> loginUser(@RequestBody @Valid LoginRequest loginRequest){
         return userService.loginUser(loginRequest);
 
+    }
+    @PostMapping("refreshtoken")
+    public ResponseEntity<?> refreshToken(@RequestBody @Valid RefreshTokenRequest refreshTokenRequest){
+        return refreshTokenService.findByToken(refreshTokenRequest.getRefreshToken())
+                .map(refreshTokenService::verifyExpiration)
+                .map(RefreshToken::getUser)
+                .map(user -> {
+                    String token = userService.jwtUtils.generateTokenFromEmailAddress(user.getEmailAddress());
+                    return ResponseEntity.status(HttpStatus.OK).body(new TokenRefreshResponse(token,refreshTokenRequest.getRefreshToken()));
+                }).orElseThrow(() -> new TokenRefreshException(refreshTokenRequest.getRefreshToken(),"refresh token is not valid"));
     }
 }
