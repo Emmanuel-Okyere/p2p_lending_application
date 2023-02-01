9090b685adbf77c7b72f0915b8a5703e2c64be33
emmanuel-gyateng <gyateng94@gmail.com>
2023-02-01 17:44:32 +0000

    Added unit test tp all files

---
+++ src/test/java/com/p2p/p2p_lending_application/loan/repository/LoanRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.p2p.p2p_lending_application.loan.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoanRepositoryTest {
+
+    @Test
+    void findByBorrowerAndApprovedIsFalse() {
+    }
+
+    @Test
+    void findAllByBorrower() {
+    }
+
+    @Test
+    void findAllByApprovedFalse() {
+    }
+}
