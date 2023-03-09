1672a59165fbac0cbcbfae1a07b0a3931d52b3f4
emmanuel-gyateng <gyateng94@gmail.com>
2023-03-09 08:25:29 +0000

    HOT FIX of the updating the profile

--- src/main/java/com/p2p/p2p_lending_application/profile/repository/TelephoneRepository.java
+++ src/main/java/com/p2p/p2p_lending_application/profile/repository/TelephoneRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TelephoneRepository extends JpaRepository<Telephone,Long> {
     List<Telephone> findAllByProfileId(UserProfile profileId);
+    Optional<Telephone> findByNumber(String number);
 
 }
